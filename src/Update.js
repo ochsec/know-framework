f43969647e1d784b3dcd23b31a1d36e522840f1b
ochsec <cochsenreither@protonmail.com>
2019-05-28 21:25:24 -0700

    initial commit

---
+++ src/Update.js
@@ -0,0 +1,19 @@
+import * as R from 'ramda'
+
+export const MSGS = { }
+
+/* Update functions */
+
+
+
+
+
+const update = (msg, model) => {
+    switch (msg.type) {
+
+    }
+
+    return model
+}
+
+export default model
---
+++ src/View.js
@@ -0,0 +1,15 @@
+import * as R from 'ramda'
+import hh from 'hyperscript-helpers'
+import { h } from 'virtual-dom'
+
+import { } from './Update'
+
+const { } = hh(h)
+
+/* View functions */
+
+const view = (dispatch, model) => {
+    return div({}, [])
+}
+
+export default view
